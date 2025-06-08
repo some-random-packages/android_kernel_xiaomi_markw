e0e1f32a0d977617c859efc2a950d4ceb75e3241
RuslanUC <dev_ruslan_uc@protonmail.com>
2025-06-08 14:41:12 +0300

    update wireguard fetch script

--- scripts/fetch-latest-wireguard.sh
+++ scripts/fetch-latest-wireguard.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 USER_AGENT="WireGuard-AndroidROMBuild/0.1 ($(uname -a))"
+WG_NAME="wireguard-monolithic-historical"
 
 [[ $(( $(date +%s) - $(stat -c %Y "net/wireguard/.check" 2>/dev/null || echo 0) )) -gt 86400 ]] || exit 0
 
-[[ $(curl -A "$USER_AGENT" -LSs https://git.zx2c4.com/WireGuard/refs/) =~ snapshot/WireGuard-([0-9.]+)\.tar\.xz ]]
+[[ $(curl -A "$USER_AGENT" -LSs https://git.zx2c4.com/$WG_NAME/refs/) =~ snapshot/$WG_NAME-([0-9.]+)\.tar\.xz ]]
 
 if [[ -f net/wireguard/version.h && $(< net/wireguard/version.h) == *${BASH_REMATCH[1]}* ]]; then
 	touch net/wireguard/.check
@@ -13,6 +14,6 @@ fi
 
 rm -rf net/wireguard
 mkdir -p net/wireguard
-curl -A "$USER_AGENT" -LsS "https://git.zx2c4.com/WireGuard/snapshot/WireGuard-${BASH_REMATCH[1]}.tar.xz" | tar -C "net/wireguard" -xJf - --strip-components=2 "WireGuard-${BASH_REMATCH[1]}/src"
+curl -A "$USER_AGENT" -LsS "https://git.zx2c4.com/$WG_NAME/snapshot/$WG_NAME-${BASH_REMATCH[1]}.tar.xz" | tar -C "net/wireguard" -xJf - --strip-components=2 "$WG_NAME-${BASH_REMATCH[1]}/src"
 sed -i 's/tristate/bool/;s/default m/default y/;' net/wireguard/Kconfig
 touch net/wireguard/.check
